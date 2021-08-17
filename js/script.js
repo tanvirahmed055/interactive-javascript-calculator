14c1e79c93a889d7c9f721e772eb1c301c3759f4
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-17 18:37:26 +0600

    added functionality for addition,subtraction,division

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+// get screen value
+const screen = document.getElementById('screen');
+
 //button click events
-const buttons = document.getElementsByClassName('buttons')
+const buttons = document.getElementsByClassName('buttons');
 for (const button of buttons) {
     button.addEventListener('click', function () {
-        console.log(button.innerText);
-    })
+        let buttonText = button.innerText;
+        if (buttonText != '=') {
+            screen.value += buttonText;
+        } else if (buttonText == '=') {
+            screen.value = eval(screen.value);
+        }
+
+    });
+
+    //console.log(button.innerText);
 }
+
